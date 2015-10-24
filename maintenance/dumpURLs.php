8855e0f0ab6973464681a0aace06b8528134d2f8
Kunal Mehta <legoktm@gmail.com>
2015-10-24 14:24:20 -0700

    Add maintenance script to generate a dump of short codes and targets
    Generates a text file dump in the same pipe-separated format as
    <http://archiveteam.org/index.php?title=URLTeam>.
    
    Bug: T116986
    Change-Id: Ibbcb06ee8654baecee1aab022ac211bba38bcd84

---
+++ maintenance/dumpURLs.php
@@ -0,0 +1,57 @@
+<?php
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+require_once "$IP/maintenance/Maintenance.php";
+
+/**
+ * Creates a pipe-separated text file of generated short codes
+ * to target URLs.
+ */
+class DumpURLs extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = 'Create a pipe-separated dump of all the short URL codes and their targets';
+		$this->addArg( 'file', 'Location to save the dump', true );
+		$this->setBatchSize( 1000 );
+	}
+
+	public function execute() {
+		$dbr = UrlShortenerUtils::getDB( DB_SLAVE );
+		$file = $this->getArg( 0 );
+		$this->output( "Writing to $file...\n" );
+		$handle = fopen( $file, 'w' );
+		$id = 0;
+		do {
+			$text = '';
+			$rows = $dbr->select(
+				'urlshortcodes',
+				array( 'usc_url', 'usc_id' ),
+				array( 'usc_id > ' . $dbr->addQuotes( $id ) ),
+				__METHOD__,
+				array( 'LIMIT' => $this->mBatchSize, 'ORDER BY' => 'usc_id ASC' )
+			);
+			foreach ( $rows as $row ) {
+				$shortCode = UrlShortenerUtils::encodeId( $row->usc_id );
+				$url = UrlShortenerUtils::convertToProtocol( $row->usc_url, PROTO_CANONICAL );
+				$text .= "{$shortCode}|{$url}\n";
+				$id = $row->usc_id;
+			}
+			$count = $rows->numRows();
+			if ( $count ) {
+				$this->output( "Writing $count entries...\n" );
+				fwrite( $handle, $text );
+			}
+		} while ( $rows->numRows() == $this->mBatchSize );
+
+		$this->output( "Done!\n" );
+		fclose( $handle );
+	}
+
+}
+
+$maintClass = "DumpURLs";
+require_once RUN_MAINTENANCE_IF_MAIN;
