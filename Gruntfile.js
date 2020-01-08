9ece82dcf2fc682b5422c35a8aa6a67d3969a5a9
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2020-01-08 10:29:27 +0000

    build: Updating npm dependencies
    * grunt-eslint: 21.0.0 → 22.0.0
    * eslint-config-wikimedia: 0.13.1 → 0.15.0
    
    Additional changes:
    * Removing manual reportUnusedDisableDirectives for eslint.
    
    Change-Id: I23e1bc2ef764344bfee972d67681825774d88f8d

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,6 @@ module.exports = function ( grunt ) {
 		},
 		eslint: {
 			options: {
-				reportUnusedDisableDirectives: true,
 				extensions: [ '.js', '.json' ],
 				cache: true
 			},
--- modules/ext.urlShortener.toolbar.js
+++ modules/ext.urlShortener.toolbar.js
@@ -1,27 +1,27 @@
 ( function () {
 	$( function () {
 		// eslint-disable-next-line no-jquery/no-global-selector
-		var shortenUrlListItem = $( '#t-urlshortener' ),
+		var $shortenUrlListItem = $( '#t-urlshortener' ),
 			api = new mw.Api();
 
-		shortenUrlListItem.on( 'click', function () {
-			var link = $( this ).find( 'a' );
-			link.text( mw.msg( 'urlshortener-url-input-submitting' ) );
+		$shortenUrlListItem.on( 'click', function () {
+			var $link = $( this ).find( 'a' );
+			$link.text( mw.msg( 'urlshortener-url-input-submitting' ) );
 
 			api.post( {
 				action: 'shortenurl',
 				url: window.location.href
 			} ).done( function ( data ) {
 				var $input = $( '<input>' ).val( data.shortenurl.shorturl );
-				shortenUrlListItem.empty().append( $input );
+				$shortenUrlListItem.empty().append( $input );
 				$input.trigger( 'focus' ).trigger( 'select' );
 			} ).fail( function () {
-				link.text( mw.msg( 'urlshortener-failed-try-again' ) );
+				$link.text( mw.msg( 'urlshortener-failed-try-again' ) );
 			} ).always( function () {
 				// Remove the click listner on the <li>
 				// On failure: the link inside points to Special:UrlShortener
 				// On success: don't trigger the API request every time the input is clicked
-				shortenUrlListItem.off( 'click' );
+				$shortenUrlListItem.off( 'click' );
 			} );
 
 			return false;
