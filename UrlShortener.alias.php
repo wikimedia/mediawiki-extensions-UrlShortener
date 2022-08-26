b37fb9d12cef0578320f2a4ff37215568c1a7ef9
Fomafix <fomafix@googlemail.com>
2022-08-26 21:56:12 +0000

    Remove LEFT-TO-RIGHT MARK (U+200E) from comments
    'LEFT-TO-RIGHT MARK' (U+200E) is not needed in the source code
    comments.
    
    c8caf26ffd9 removed already the LRM/RLM from Names.php in core.
    
    Change-Id: I7fa54b50094464a56f4e54f4d3e8fdd82c1b2eee

--- UrlShortener.alias.php
+++ UrlShortener.alias.php
@@ -49,12 +49,12 @@ $specialPageAliases['mk'] = [
 	'UrlShortener' => [ 'СкратувачНаURL' ],
 ];
 
-/** Serbian (Cyrillic script) (српски (ћирилица)‎) */
+/** Serbian (Cyrillic script) (српски (ћирилица)) */
 $specialPageAliases['sr-ec'] = [
 	'UrlShortener' => [ 'Скраћивач_адреса', 'Скраћивач_URL_адреса' ],
 ];
 
-/** Serbian (Latin script) (srpski (latinica)‎) */
+/** Serbian (Latin script) (srpski (latinica)) */
 $specialPageAliases['sr-el'] = [
 	'UrlShortener' => [ 'Skraćivač_adresa', 'Skraćivač_URL_adresa' ],
 ];
@@ -64,12 +64,12 @@ $specialPageAliases['ur'] = [
 	'UrlShortener' => [ 'یوآرایل_مختصر_کنندہ' ],
 ];
 
-/** Simplified Chinese (中文（简体）‎) */
+/** Simplified Chinese (中文（简体）) */
 $specialPageAliases['zh-hans'] = [
 	'UrlShortener' => [ 'URL缩短程序' ],
 ];
 
-/** Traditional Chinese (中文（繁體）‎) */
+/** Traditional Chinese (中文（繁體）) */
 $specialPageAliases['zh-hant'] = [
 	'UrlShortener' => [ 'URL縮短程式' ],
 ];
