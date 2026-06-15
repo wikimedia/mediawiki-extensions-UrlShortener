5fff60216b4c2e0a4eec608768cdf4892050e119
Ed Sanders <esanders@wikimedia.org>
2026-06-15 13:08:46 +0100

    Grunt: Use .eslintignore
    Change-Id: Id5a105b5cbece1d2fbff50a016dec977f6b6b723

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,11 +24,7 @@ module.exports = function ( grunt ) {
 				cache: true,
 				fix: grunt.option( 'fix' )
 			},
-			all: [
-				'**/*.{js,json}',
-				'!node_modules/**',
-				'!vendor/**'
-			]
+			all: [ '.' ]
 		},
 		banana: conf.MessagesDirs,
 		stylelint: {
